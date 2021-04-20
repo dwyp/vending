df67f1caf0b139ab41e97e897c6d86a0e2293106
daniel.wypiszynski <daniel.wypiszynski@cardinalhealth.com>
2021-04-20 12:20:08 -0400

    Use better Assertions

--- src/test/java/com/kata/practice/PracticeApplicationTests.java
+++ src/test/java/com/kata/practice/PracticeApplicationTests.java
@@ -1,14 +1,13 @@
 package com.kata.practice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class PracticeApplicationTests {
 
     @Test
     void testHelloWorld() {
         PracticeApplication practiceApplication = new PracticeApplication();
-        assert practiceApplication.getText().equals("Hello world!");
+        Assertions.assertEquals("Hello world!", practiceApplication.getText());
     }
-
 }
